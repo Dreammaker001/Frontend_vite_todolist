1deb78e11d18978f93e385badb1f593b93f99609
Dreammaker001 <bastiansensara24@gmail.com>
2021-06-07 14:23:15 +0700

    install vue router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Todo msg="Hello Vue 3 + Vite" />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
